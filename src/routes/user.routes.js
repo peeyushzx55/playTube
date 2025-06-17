a75cfcea5dfeac87c519ec906ea1979c3d9d0b56
PEEYUSH KUMAR <peeyush.buisness45@gmail.com>
2025-06-18 00:23:08 +0530

    implement user authentication with login and logout functionality, including JWT verification

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,11 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controllers.js";
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+} from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middlewares.js";
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
 const router = Router();
 
@@ -14,5 +19,9 @@ router.route("/register").post(
   ]),
   registerUser
 );
+
+router.route("/login").post(loginUser);
+
+router.route("/logout").post(verifyJWT, logoutUser);
 
 export default router;
